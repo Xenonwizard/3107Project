4048d04d6a268311692c6a88f0fc43f5703f0e75
Ryan Ho <ryyisryy@gmail.com>
2025-03-25 02:13:24 +0800

    add airflow ETL to GCP bucket

---
+++ Airflow/dags/ETL.py
@@ -0,0 +1,84 @@
+from airflow.decorators import dag, task
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from kaggle.api.kaggle_api_extended import KaggleApi
+from datetime import datetime, timedelta
+import os
+import pandas as pd
+import pendulum
+import re
+
+@dag(dag_id="hospitality_reviews_etl_taskflow", start_date=pendulum.datetime(2025, 1, 1), schedule="@daily", catchup=False, tags=["project"])
+def hospitality_reviews_taskflow_api_etl():
+    @task
+    def extract_data(dataset_name: str):
+        api = KaggleApi()
+        api.authenticate()
+
+        download_path = "/tmp/base_dataset"
+        api.dataset_download_files(
+            dataset=dataset_name,
+            path=download_path,
+            unzip=True,
+        )
+
+        downloaded_files = [os.path.join(download_path, file) for file in os.listdir(download_path) if ".csv" in file]
+
+        return downloaded_files[0]
+    
+    @task
+    def split_duplicates(dataset_path: str):
+        df = pd.read_csv(dataset_path)
+
+        duplicates = df.duplicated(["Hotel_Address", "Negative_Review", "Positive_Review", "Reviewer_Score"], keep=False)
+        duplicated_reviews = df[duplicates]
+        os.makedirs("/tmp/duplicated_dataset", exist_ok=True)
+        duplicated_reviews.to_csv("/tmp/duplicated_dataset/Duplicated_Reviews.csv", index=False)
+
+        return "/tmp/duplicated_dataset/Duplicated_Reviews.csv"
+    
+    def clean_reviews(text):
+        # Convert to lowercase
+        text = text.lower()
+        # Remove punctuation
+        text = re.sub(r'[^\w\s]', '', text)
+        return text
+    
+    @task
+    def clean_data(dataset_path: str):
+        df = pd.read_csv(dataset_path)
+
+        df["Negative_Review"] = df["Negative_Review"].apply(clean_reviews)
+        df["Positive_Review"] = df["Positive_Review"].apply(clean_reviews)
+        df["Review"] = df["Negative_Review"] + " " + df["Positive_Review"]
+
+        df.drop(axis=1, columns=["Negative_Review", "Positive_Review", "Review_Total_Negative_Word_Counts", "Review_Total_Positive_Word_Counts", "days_since_review"], inplace=True)
+        df.to_csv(dataset_path, index=False)
+        
+        return dataset_path
+
+    @task
+    def load_data(dataset_path: str, bucket_name: str):    
+
+        upload_to_gcs = LocalFilesystemToGCSOperator(
+            task_id="upload_file_to_gcs",
+            src=dataset_path,
+            dst=dataset_path.split("/")[-1],
+            bucket=bucket_name,
+            gcp_conn_id="google_cloud_default",
+        )
+    
+        upload_to_gcs.execute(context={})
+    
+    dataset_path = extract_data("jiashenliu/515k-hotel-reviews-data-in-europe")
+    # Dataset that contains duplicated reviews (Possibly use for training)
+    duplicates_path = split_duplicates(dataset_path)
+
+    dataset_path = clean_data.override(task_id="clean_all_reviews")(dataset_path)
+    duplicates_path = clean_data.override(task_id="clean_duplicated_reviews")(duplicates_path)
+
+    bucket_name = "is3107_hospitality_reviews_bucket"
+    load_data.override(task_id="load_all_reviews")(dataset_path, bucket_name)
+    load_data.override(task_id="load_duplicated_reviews")(duplicates_path, bucket_name)
+
+
+project_etl_dag = hospitality_reviews_taskflow_api_etl()
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 3107Project
+
+## Running Airflow
+```
+docker compose up airflow-init  
+docker compose up
+```
+
+## GCP Connection
+To link GCP with Airflow, within the Airflow Dashboard (Admin > Connections) add a new connection record:
+- Connection Id: google_cloud_default
+- Connection Type: Google Cloud
+- Keyfile Path: /opt/airflow/config/AirflowServiceAccount.json
