82c971071f1a4e4cc533774318ff813362cc53b8
Ryan Ho <ryyisryy@gmail.com>
2025-04-23 16:43:43 +0800

    add step to transfer gcs to bigquery

--- Airflow/dags/WebScraper.py
+++ Airflow/dags/WebScraper.py
@@ -1,10 +1,9 @@
 from airflow.decorators import dag, task
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
-from airflow.operators.python import get_current_context
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from webscraper.BookingReviewSpider import BookingReviewSpider
-from dateutil.relativedelta import relativedelta
 import xml.etree.ElementTree as ET
 import pandas as pd
 from datetime import datetime
@@ -17,7 +16,7 @@ import re
 import string
 import dateparser
 
-@dag(dag_id="webscraper_taskflow", start_date=pendulum.datetime(2025, 1, 1), schedule="@monthly", catchup=False, tags=["project"])
+@dag(dag_id="webscraper_taskflow", start_date=pendulum.datetime(2025, 1, 1), schedule="@daily", catchup=False, tags=["project"])
 def webscraper_taskflow_api():
     @task
     def extract_sitemap(sitemap_url: str):
@@ -66,17 +65,11 @@ def webscraper_taskflow_api():
         file_path = "/tmp/review_urls.json"
         with open(file_path, "w") as f:
             # ! Remove slice to scrape reviews of all hotels in the gz files
-            json.dump(all_review_urls[0:100], f)
+            json.dump(all_review_urls[0:10], f)
         return file_path
     
     @task
-    def run_scrapy_spider(json_path: str, ignore_months=False):
-        context = get_current_context()
-        execution_date = context["execution_date"]
-        
-        # Calculate previous month's start/end dates
-        prev_month_end = execution_date.replace(day=1) - relativedelta(days=1)
-        prev_month_start = prev_month_end.replace(day=1)
+    def run_scrapy_spider(json_path: str):
 
         # Configure Scrapy with custom settings
         settings = get_project_settings()        
@@ -110,9 +103,6 @@ def webscraper_taskflow_api():
         process.crawl(
             BookingReviewSpider,
             input_file=json_path,
-            ignore_months=ignore_months,
-            prev_month_start=prev_month_start.date(),
-            prev_month_end=prev_month_end.date()
         )
         process.start()
 
@@ -125,6 +115,8 @@ def webscraper_taskflow_api():
         
         # Remove Unicode characters (non-ASCII)
         text = re.sub(r'[^\x00-\x7F]+', '', text)
+        text = text.replace('\n', ' ')
+        text = re.sub(r'\s+', ' ', text)
         
         translator = str.maketrans('', '', string.punctuation)
         text = text.translate(translator)
@@ -153,7 +145,7 @@ def webscraper_taskflow_api():
             return None
     
     @task
-    def clean_data(reviews_path: str, only_current_date=True):
+    def clean_data(reviews_path: str):
         df = pd.read_json(reviews_path)
 
         for column in df.columns:
@@ -174,7 +166,7 @@ def webscraper_taskflow_api():
         return reviews_path.split(".json")[0] + ".csv"
     
     @task
-    def load_data(dataset_path: str, bucket_name: str):    
+    def load_data(dataset_path: str, bucket_name: str, big_query_table_name: str, **context):    
 
         timestamp = datetime.now().strftime("%Y%m%d")
         file_name = dataset_path.split("/")[-1]
@@ -189,16 +181,47 @@ def webscraper_taskflow_api():
         )
     
         upload_to_gcs.execute(context={})
+
+        load_to_big_query = GCSToBigQueryOperator(
+            task_id="gcs_to_bigquery",
+            bucket=bucket_name,
+            source_objects=[timestamped_file_name],
+            destination_project_dataset_table=big_query_table_name,
+            schema_fields=[
+                {"name": "Hotel_Address", "type": "STRING", "mode": "NULLABLE"},
+                {"name": "Review_Date", "type": "DATE", "mode": "NULLABLE"},
+                {"name": "Average_Score", "type": "FLOAT", "mode": "NULLABLE"},
+                {"name": "Hotel_Name", "type": "STRING", "mode": "NULLABLE"},
+                {"name": "Reviewer_Nationality", "type": "STRING", "mode": "NULLABLE"},
+                {"name": "Negative_Review", "type": "STRING", "mode": "NULLABLE"},
+                {"name": "Positive_Review", "type": "STRING", "mode": "NULLABLE"},
+                {"name": "Reviewer_Score", "type": "FLOAT", "mode": "NULLABLE"},
+                {"name": "Tags", "type": "STRING", "mode": "NULLABLE"},
+            ],
+            write_disposition="WRITE_APPEND",
+        )
+
+        ti = context.get('ti')
+
+        load_to_big_query.execute(context= {
+            'ti': ti,
+            'logical_date': datetime.now(),
+            'task': context.get('task'),
+            'dag': context.get('dag'),
+            'run_id': context.get('run_id'),
+            'task_instance': ti,
+        })
+
         os.remove(dataset_path)
 
     gz_urls = extract_sitemap("https://www.booking.com/sitembk-hotel-review-index.xml")
     reviews_json_path = download_gz_files(gz_urls)
-    # ! Remove ignore_months when scraping on a monthly basis (Currently it will ignore the month requirement)
-    scraped_reviews_json_path = run_scrapy_spider(reviews_json_path, ignore_months=True)
+    scraped_reviews_json_path = run_scrapy_spider(reviews_json_path)
     scraped_reviews_json_path = clean_data(scraped_reviews_json_path)
 
     bucket_name = "is3107_hospitality_reviews_bucket"
-    load_data(scraped_reviews_json_path, bucket_name)
+    big_query_table_name = "hotel_reviews.reviews"
+    load_data(scraped_reviews_json_path, bucket_name, big_query_table_name)
 
 
 project_etl_dag = webscraper_taskflow_api()
